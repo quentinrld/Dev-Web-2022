f008961476aa36c92e7663685998ca68c95c24fc
Antoine Geonet <antoinegeonet@MacBook-Air-de-Antoine.local>
2022-03-21 17:34:09 +0100

    demo backend v3

--- demo_backend/index.js
+++ demo_backend/index.js
@@ -1,33 +1,44 @@
 const express = require('express');
-const {urlencoded} = require("express");
+const {urlencoded, json} = require("express");
 const {readFile} = require('fs').promises;
 
 const app = express();
+
+const products = [
+    {
+        name: "car",
+        id: 0
+    },
+    {
+        name: "computer",
+        id: 1
+    },
+    {
+        name: "kitchen",
+        id: 2
+    }
+];
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 // Functions
-async function renderMainPage() {
-    return readFile(__dirname + '/main.html', 'utf-8')
-}
 
-async function renderThanksPage() {
-    return readFile(__dirname + '/thanks.html', 'utf-8')
-}
 
 // API routes
-app.get('/', async (request, response) => {
-    response.send(await renderMainPage());
+
+app.get('/products/:id', (req, res) => {
+    res.json(products.find((element) => {
+        return +req.params.id === element.id; // le + pour convertir le str en number
+    }))
 })
 
-app.post('/formResp', async (request, response) => {
-    console.log(request.body);
-    response.send(await renderThanksPage());
+app.post('/testPost', async (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
     }
 )
 
 // Port listening
 app.listen(process.env.PORT || 3000, () => console.log('App is available on http://localhost:3000'));
-
